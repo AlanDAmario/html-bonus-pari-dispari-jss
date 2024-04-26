c18d8a280e3355690dd132502472abac85227010
AlanDAmario <vega93@live.it>
2024-04-26 18:05:32 +0200

    first commit

---
+++ jss/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//SCELTA DELL UTENTE PARI O DISPARI//
+const userChoice = prompt('pari i odispari');
+console.log(userChoice);
+
+//SCELTA NUMERO UTENTE//
+const userNumber = Number(prompt('scegli un mnumero tra 1 e 9'));
+console.log(userNumber);
+
+
+//PARI//
+
+
+
